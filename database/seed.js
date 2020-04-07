ff50e93cb87c1f83e276549fa5c8288918d82b8a
Nicholas Buzzerio <nick.buzzerio@gmail.com>
2020-04-06 23:51:46 -0400

    began scaling data-generation from 400 to 40M but it takes too long or runs out of memory...need to find a more efficient way

--- database/seed.js
+++ database/seed.js
@@ -1,42 +1,55 @@
 /* eslint-disable camelcase */
 const faker = require('faker');
-const Comments = require('./index');
+// const Comments = require('./index');
 
 const comments = [];
+// const user_icons = [];
+// const user_names = [];
 
-for (let i = 0; i < 400; i += 1) {
-  const song_id = faker.random.number({
-    min: 1,
-    max: 100,
-  });
-  const user_id = faker.random.number({
-    min: 1,
-    max: 100,
-  });
+// //generate 1000 images to pick from
+// for (var i = 0; i < 1000; i++) {
+//   user_icons.push(faker.image.avatar());
+//   user_names.push(faker.internet.userName());
+// }
 
-  const user_icon = faker.image.avatar();
+for (var j = 0; j < 1000; j++) {
+  //creates 40000 records in this loop
+  for (let i = 40000 * j; i < 40000 * j + 40000; i++) { //scaled from 400 to 10M
+    const song_id = faker.random.number({
+      min: 1,
+      max: 100,
+    });
+    const user_id = faker.random.number({
+      min: 1,
+      max: 100,
+    });
+    
+    const randomUser = Math.floor(Math.random() * 1000);
+    const user_icon = faker.image.avatar();
+    const user_name = faker.internet.userName();
 
-  const user_name = faker.internet.userName();
+    const posted_at = faker.date.recent();
 
-  const posted_at = faker.date.recent();
+    const randomWordCount = Math.floor(Math.random() * 30);
+    const message = faker.random.words(randomWordCount);
 
-  const randomWordCount = Math.floor(Math.random() * 30);
-  const message = faker.random.words(randomWordCount);
-
-  const audio_position = faker.random.number({
-    min: 1,
-    max: 80,
-  });
-  const newComment = {
-    comment_id: i,
-    song_id,
-    user_id,
-    user_name,
-    user_icon,
-    message,
-    audio_position,
-    posted_at,
-  };
-  comments.push(newComment);
+    const audio_position = faker.random.number({
+      min: 1,
+      max: 80,
+    });
+    const newComment = {
+      comment_id: i,
+      song_id,
+      user_id,
+      user_name,
+      user_icon,
+      message,
+      audio_position,
+      posted_at,
+    };
+    comments.push(newComment);
+  }
+console.log('i: ', 40000 * j, 'j: ', j)
+// console.log(comments);
 }
-Comments.insertMany(comments);
+// Comments.insertMany(comments);
