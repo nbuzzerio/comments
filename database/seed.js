09da7f3c5a82487962e55689b0da90bc515534b1
Nicholas Buzzerio <nick.buzzerio@gmail.com>
2020-04-07 02:22:43 -0400

    scaled up to 8M records but trying to exceed that results in memory error

--- database/seed.js
+++ database/seed.js
@@ -3,40 +3,45 @@ const faker = require('faker');
 // const Comments = require('./index');
 
 const comments = [];
-// const user_icons = [];
-// const user_names = [];
+const messages = [];
 
-// //generate 1000 images to pick from
-// for (var i = 0; i < 1000; i++) {
-//   user_icons.push(faker.image.avatar());
-//   user_names.push(faker.internet.userName());
-// }
+console.time('1000 msgs: ')
+for (var i = 0; i < 1000; i++) {
+  const randomWordCount = Math.floor(Math.random() * 30);
+  const message = faker.random.words(randomWordCount);
+  messages.push(message);
+}
+console.timeEnd('1000 msgs: ');
 
-for (var j = 0; j < 1000; j++) {
-  //creates 40000 records in this loop
+console.time('40M records: ')
+for (var j = 0; j < 200; j++) {
+
   for (let i = 40000 * j; i < 40000 * j + 40000; i++) { //scaled from 400 to 10M
+
     const song_id = faker.random.number({
       min: 1,
-      max: 100,
+      max: 10000000,
     });
+
     const user_id = faker.random.number({
       min: 1,
-      max: 100,
+      max: 10000000,
     });
-    
-    const randomUser = Math.floor(Math.random() * 1000);
+
     const user_icon = faker.image.avatar();
+    
     const user_name = faker.internet.userName();
 
     const posted_at = faker.date.recent();
-
-    const randomWordCount = Math.floor(Math.random() * 30);
-    const message = faker.random.words(randomWordCount);
 
     const audio_position = faker.random.number({
       min: 1,
       max: 80,
     });
+
+    //picks a random message from the array
+    message = messages[Math.floor(Math.random() * 1000)];
+    
     const newComment = {
       comment_id: i,
       song_id,
@@ -47,9 +52,13 @@ for (var j = 0; j < 1000; j++) {
       audio_position,
       posted_at,
     };
+
     comments.push(newComment);
+
   }
-console.log('i: ', 40000 * j, 'j: ', j)
-// console.log(comments);
+
 }
+
+console.timeEnd('40M records: ')
+// console.log(comments);
 // Comments.insertMany(comments);
